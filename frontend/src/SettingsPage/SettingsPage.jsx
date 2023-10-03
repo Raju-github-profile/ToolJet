a6431c188504fc76e4c779959d2ff8c3042e6368
Raju kadel <108010466+Raju-github-profile@users.noreply.github.com>
2023-10-04 01:24:16 +0545

    Revert "Update SettingsPage.jsx"

--- frontend/src/SettingsPage/SettingsPage.jsx
+++ frontend/src/SettingsPage/SettingsPage.jsx
@@ -197,9 +197,6 @@ function SettingsPage(props) {
                             if (Math.round(file.size / 1024) > 2048) {
                               toast.error('File size cannot exceed more than 2MB');
                               e.target.value = null;
-                            } else if (!file.type.startsWith('image/')) {
-                              toast.error('Please select an image file');
-                              e.target.value = null;
                             } else {
                               setSelectedFile(file);
                             }
